0e071143fa3a254a80512e12d8fcae54d8f09dd3
ascyazilim <yazilimasc@gmail.com>
2024-05-15 20:05:07 +0300

    added repository

--- backend/task-management-system/src/main/java/org/asc/taskmanagementsystem/repository/TaskRepository.java
+++ backend/task-management-system/src/main/java/org/asc/taskmanagementsystem/repository/TaskRepository.java
@@ -4,4 +4,5 @@ import org.asc.taskmanagementsystem.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
 }
