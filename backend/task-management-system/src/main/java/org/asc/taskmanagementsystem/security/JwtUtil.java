3b195e42f5406e5d281f53f31ac21759a1941be8
ascyazilim <yazilimasc@gmail.com>
2024-05-15 20:37:07 +0300

    JwtUtil added

---
+++ backend/task-management-system/src/main/java/org/asc/taskmanagementsystem/security/JwtUtil.java
@@ -0,0 +1,35 @@
+package org.asc.taskmanagementsystem.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String secret = "secret";
+    private long expiration = 604800000L; //1 week
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+ expiration))
+                .signWith(SignatureAlgorithm.HS512,secret)
+                .compact();
+    }
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
