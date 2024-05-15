ac34d4ad5e555db520364232ceef8139d6b21d2d
ascyazilim <yazilimasc@gmail.com>
2024-05-15 20:42:56 +0300

    CustomUserDetailsService added

---
+++ backend/task-management-system/src/main/java/org/asc/taskmanagementsystem/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package org.asc.taskmanagementsystem.service;
+
+import org.asc.taskmanagementsystem.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        org.asc.taskmanagementsystem.model.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword()
+        , new ArrayList<>());
+    }
+}
